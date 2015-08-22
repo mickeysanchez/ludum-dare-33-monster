73e092af6f642429fa31c8526d73a297cde78cc0
mickeysanchez <sanchez.mickey@gmail.com>
2015-08-22 00:32:46 -0400

    grid selector

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,5 +2,40 @@
 
 angular.module('monsterApp')
   .controller('MainCtrl', function($scope, $http, socket) {
-    $scope.grid = new Array(400);
+    $scope.numSquares = 10;
+    $scope.calcPercent = 100 / $scope.numSquares;
+
+    $scope.selected = {
+      is_selected: false
+    };
+
+    $scope.select = function(newlySelected) {
+      $scope.selected.is_selected = false;
+      newlySelected.is_selected = true;
+      $scope.selected = newlySelected;
+    }
+
+    $scope.createSquare = function(x, y) {
+      return {
+        is_selected: false,
+        style: {
+          background: 'grey',
+          width: $scope.calcPercent + "%"
+        }
+      }
+    }
+
+    $scope.newGrid = function() {
+      var grid = [];
+      for (var i = 0; i < $scope.numSquares; i++) {
+        var row = []
+        for (var j = 0; j < $scope.numSquares; j++) {
+          row.push($scope.createSquare(j, i))
+        }
+        grid.push(row)
+      }
+      return grid;
+    }
+
+    $scope.grid = $scope.newGrid();
   });
